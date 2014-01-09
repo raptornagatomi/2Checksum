fd0111eb98655e0fda3ee93dc3208ea8e9336487
Raptor <raptor.evo@gmail.com>
2014-01-10 01:03:12 +0800

    Add version label

--- 2Checksum/Form1.Designer.cs
+++ 2Checksum/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.RichTextBox_FileInfo = new System.Windows.Forms.RichTextBox();
             this.CheckBox_Verbose = new System.Windows.Forms.CheckBox();
             this.PictureBox_Logo = new System.Windows.Forms.PictureBox();
+            this.Label_Version = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,9 +43,9 @@
             // 
             this.Button_Browse.Image = ((System.Drawing.Image)(resources.GetObject("Button_Browse.Image")));
             this.Button_Browse.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Button_Browse.Location = new System.Drawing.Point(325, 78);
+            this.Button_Browse.Location = new System.Drawing.Point(341, 78);
             this.Button_Browse.Name = "Button_Browse";
-            this.Button_Browse.Size = new System.Drawing.Size(109, 36);
+            this.Button_Browse.Size = new System.Drawing.Size(117, 36);
             this.Button_Browse.TabIndex = 0;
             this.Button_Browse.Text = "Browse";
             this.Button_Browse.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -55,9 +56,9 @@
             // 
             this.Button_Copy.Image = ((System.Drawing.Image)(resources.GetObject("Button_Copy.Image")));
             this.Button_Copy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Button_Copy.Location = new System.Drawing.Point(325, 36);
+            this.Button_Copy.Location = new System.Drawing.Point(341, 36);
             this.Button_Copy.Name = "Button_Copy";
-            this.Button_Copy.Size = new System.Drawing.Size(109, 36);
+            this.Button_Copy.Size = new System.Drawing.Size(117, 36);
             this.Button_Copy.TabIndex = 1;
             this.Button_Copy.Text = "Copy";
             this.Button_Copy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -78,7 +79,7 @@
             this.RichTextBox_FileInfo.Name = "RichTextBox_FileInfo";
             this.RichTextBox_FileInfo.ReadOnly = true;
             this.RichTextBox_FileInfo.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.RichTextBox_FileInfo.Size = new System.Drawing.Size(307, 156);
+            this.RichTextBox_FileInfo.Size = new System.Drawing.Size(323, 156);
             this.RichTextBox_FileInfo.TabIndex = 6;
             this.RichTextBox_FileInfo.Text = "";
             this.RichTextBox_FileInfo.WordWrap = false;
@@ -88,7 +89,7 @@
             this.CheckBox_Verbose.AutoSize = true;
             this.CheckBox_Verbose.Checked = true;
             this.CheckBox_Verbose.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckBox_Verbose.Location = new System.Drawing.Point(325, 12);
+            this.CheckBox_Verbose.Location = new System.Drawing.Point(341, 12);
             this.CheckBox_Verbose.Name = "CheckBox_Verbose";
             this.CheckBox_Verbose.Size = new System.Drawing.Size(75, 18);
             this.CheckBox_Verbose.TabIndex = 7;
@@ -99,11 +100,22 @@
             // PictureBox_Logo
             // 
             this.PictureBox_Logo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox_Logo.Image")));
-            this.PictureBox_Logo.Location = new System.Drawing.Point(352, 120);
+            this.PictureBox_Logo.Location = new System.Drawing.Point(355, 120);
             this.PictureBox_Logo.Name = "PictureBox_Logo";
             this.PictureBox_Logo.Size = new System.Drawing.Size(48, 48);
             this.PictureBox_Logo.TabIndex = 8;
             this.PictureBox_Logo.TabStop = false;
+            // 
+            // Label_Version
+            // 
+            this.Label_Version.AutoSize = true;
+            this.Label_Version.Font = new System.Drawing.Font("Cataneo BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Version.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Label_Version.Location = new System.Drawing.Point(409, 151);
+            this.Label_Version.Name = "Label_Version";
+            this.Label_Version.Size = new System.Drawing.Size(49, 17);
+            this.Label_Version.TabIndex = 9;
+            this.Label_Version.Text = "v1.0.0.0";
             // 
             // Form1
             // 
@@ -111,7 +123,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(440, 178);
+            this.ClientSize = new System.Drawing.Size(470, 178);
+            this.Controls.Add(this.Label_Version);
             this.Controls.Add(this.PictureBox_Logo);
             this.Controls.Add(this.CheckBox_Verbose);
             this.Controls.Add(this.RichTextBox_FileInfo);
@@ -141,6 +154,7 @@
         private System.Windows.Forms.RichTextBox RichTextBox_FileInfo;
         private System.Windows.Forms.CheckBox CheckBox_Verbose;
         private System.Windows.Forms.PictureBox PictureBox_Logo;
+        private System.Windows.Forms.Label Label_Version;
     }
 }
 
